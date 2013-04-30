7165fa7903558144a2e4df0893d044f1a54fbe85
kimmanis <kimmanis@gmail.com>
2013-04-29 17:05:59 -0700

    heroku

--- config/application.rb
+++ config/application.rb
@@ -73,7 +73,6 @@ module Workflow
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.compile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '2.0'
