0b1d1d768f4fb564b98d486ba01ac343651bd805
kimmanis <kimmanis@gmail.com>
2013-04-29 17:17:51 -0700

    precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -73,6 +73,7 @@ module Workflow
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '2.0'
