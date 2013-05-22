92e9dcd1250ca71704b1800b5ef63dfc2cd1d226
kimmanis <kimmanis@gmail.com>
2013-05-21 23:16:41 -0700

    form redesign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_form", :f => builder)
     end
-    button_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    button_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class=>"button secondary small")
   end
 end
