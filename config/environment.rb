4cf1a7283b22ed8a6d6abd9788b8811bb6f4cb3a
kimmanis <kimmanis@gmail.com>
2013-06-09 14:50:49 -0700

    fixed #127

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ Workflow::Application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   if instance.error_message.kind_of?(Array)
     %(<div class="form-field error">#{html_tag}<small class="error">&nbsp;
-      #{instance.error_message.join(',')}</small></div).html_safe
+      #{instance.error_message.join(',')}</small></div>).html_safe
   else
     %(<div class="form-field error">#{html_tag}<small class="error">&nbsp;
-      #{instance.error_message}</small></div).html_safe
+      #{instance.error_message}</small></div>).html_safe
   end
 end
