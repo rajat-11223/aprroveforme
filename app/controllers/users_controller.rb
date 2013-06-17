22c8745832345749a716654a3582ee908bb3293e
kimmanis <kimmanis@gmail.com>
2013-06-16 22:18:19 -0700

    #139 sortable almost done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?, :except => [:index]
+  helper_method :sort_column, :sort_direction
 
   def index
     unless current_user.has_role? :admin
       redirect_to root_url, :alert => "Access denied."
     end
-      @users = User.order(:created_at).page params[:page]
+      @users = User.order(sort_column + " " + sort_direction).page params[:page]
 
   end
 
@@ -24,8 +25,18 @@ class UsersController < ApplicationController
   end
 
 
-def show
+  def show
     @user = User.find(params[:id])
+  end
+
+  private
+  
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
