7338eac4ddd47224685188ae026298541315d8a9
HTP <htp@htp-desktop.(none)>
2014-04-18 11:53:48 +0530

    refers to chnages on Omni outh cred

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { :scope => ENV['GOOGLE_SCOPE'], :approval_prompt => "auto"}
+  provider :google_oauth2, "79834970872-dchs42oupidlep22vf39s0lcdk0uv1dc.apps.googleusercontent.com
+", "NBwr01IaHgWWCNMhm3vPaG7f", { :scope => ENV['GOOGLE_SCOPE'], :approval_prompt => "auto"}
 end
 
 #:approval_prompt => "auto"
