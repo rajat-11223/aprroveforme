52368793f8d65c90f1ee219d0b5756d8835f4b5e
HTP <htp@htp-desktop.(none)>
2014-04-18 12:07:29 +0530

    refres to chnages in config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "79834970872-dchs42oupidlep22vf39s0lcdk0uv1dc.apps.googleusercontent.com
-", "NBwr01IaHgWWCNMhm3vPaG7f", { :scope => ENV['GOOGLE_SCOPE'], :approval_prompt => "auto"}
+  provider :google_oauth2, "79834970872-0i16kifmu100tf9af5orbqg8fm30o7ke.apps.googleusercontent.com
+", "Xs-PuaCjK6iAtuLRUzj3f0NY", {  :approval_prompt => "auto"}
 end
 
 #:approval_prompt => "auto"
