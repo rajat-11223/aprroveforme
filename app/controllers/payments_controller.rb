bcbf5950327b33ec7391b86dc013ca62119dad2c
Ricky Chilcott <ricky@rakefire.io>
2018-08-15 14:41:31 -0400

    Simplify PaymentsController and have it load

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,16 +1,13 @@
 class PaymentsController < ApplicationController
   def new
     authorize! :create, Subscription.new(user: current_user)
+    @amount = calculate_amount
 
-    if !Subscription.exist?(user_id: current_user.id)
-      @amount = calculate_amount
-    else
-      if session[:upgrade]== "upgrade"
-        if Subscription.find_by(user_id: current_user.id).plan_type == "unlimited"
-          session[:degrade]="degrade"
-        end
+    if Subscription.exist?(user_id: current_user.id)
+      if session[:upgrade] == "upgrade"
+        current_user.reload
 
-        @amount = calculate_amount
+        session[:degrade] = "degrade" if (current_user.subscription.plan_type == "unlimited")
       else
         redirect_to root_url
       end
@@ -20,34 +17,37 @@ class PaymentsController < ApplicationController
   def confirm
     authorize! :update, current_user.subscription
 
+    @amount = calculate_amount
     @result = Braintree::TransparentRedirect.confirm(request.query_string)
+
     if @result.success?
-
       if session[:upgrade] == "upgrade"
-        @subscription = Subscription.find_by(user_id: current_user.id)
-        @subscription.plan_type = session[:plan_type]
-        @subscription.plan_date = Date.today
-        @subscription.save
-        session[:plan_type] = nil
-        session[:upgrade] = nil
+        current_user.reload
 
-          if session[:degrade] != "degrade"
-            redirect_to root_url, notice: 'Congratulations, you have successfully updated your plan.'
+        current_user.subscription.update_attributes! plan_type: session[:plan_type],
+                                                     plan_date: Date.today
+        session.delete(:plan_type)
+        session.delete(:upgrade)
+
+        msg =
+          if session.delete(:degrade) != "degrade"
+            'Congratulations, you have successfully updated your plan.'
           else
-            session[:degrade] = nil
-            redirect_to root_url, notice: 'Congratulations, you have successfully downgraded your plan.'
+            'Congratulations, you have successfully downgraded your plan.'
           end
-        end
+
+        redirect_to root_url, notice: msg
       else
-        # Subscription.create!(plan_type: session[:plan_type], plan_date: Date.today, user_id: current_user.id)
-        binding.pry
+        current_user.reload
         current_user.subscription.update_attributes! plan_type: session[:plan_type], plan_date: Date.today
+
         SubscriptionHistory.create!(user: current_user, plan_type: session[:plan_type], plan_date: Date.today)
         session[:plan_type] = nil
+
         redirect_to root_url
       end
     else
-      @amount = calculate_amount
+
       render action: "new"
     end
   end
