de0d1323546de9f1e720c5d3c89dce32cefe3109
Ricky Chilcott <ricky@rakefire.io>
2018-08-23 12:05:19 -0400

    Add specs to static pages when logged in

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,37 +1,75 @@
 require "rails_helper"
 
-describe "Navigate to static pages" do
+describe "Navigate to static page" do
   it 'home' do
-    visit_page_with_content(path: root_path, content: "ApproveForMe")
+    visit_page_with_content(visit_path: root_path, content: "ApproveForMe")
   end
 
   it "how it works" do
-    visit_page_with_content(path: page_path("how-it-works"), content: "How It Works")
+    visit_page_with_content(visit_path: page_path("how-it-works"), content: "How It Works")
   end
 
   it "pricing" do
-    visit_page_with_content(path: page_path("pricing"), content: "Pricing")
+    visit_page_with_content(visit_path: page_path("pricing"), content: "Pricing")
   end
 
   it "about" do
-    visit_page_with_content(path: page_path("about"), content: "About")
+    visit_page_with_content(visit_path: page_path("about"), content: "About")
   end
 
   it "faq" do
-    visit_page_with_content(path: page_path("faq"), content: "Frequently Asked Questions")
+    visit_page_with_content(visit_path: page_path("faq"), content: "Frequently Asked Questions")
   end
 
   it "terms" do
-    visit_page_with_content(path: page_path("terms"), content: "Terms of Service")
+    visit_page_with_content(visit_path: page_path("terms"), content: "Terms of Service")
   end
 
-  it "priacy" do
-    visit_page_with_content(path: page_path("privacy"), content: "Privacy Policy")
+  it "privacy" do
+    visit_page_with_content(visit_path: page_path("privacy"), content: "Privacy Policy")
+  end
+
+  context "when signed in visiting" do
+    let(:user) { create(:user) }
+
+    before do
+      mock_omniauth_provider!(user: user)
+      visit root_path
+      click_link "Login or Signup"
+    end
+
+    it 'home' do
+      visit_page_with_content(visit_path: root_path, result_path: dashboard_home_index_path, content: "ApproveForMe")
+    end
+
+    it "how it works" do
+      visit_page_with_content(visit_path: page_path("how-it-works"), content: "How It Works")
+    end
+
+    it "pricing" do
+      visit_page_with_content(visit_path: page_path("pricing"), content: "Pricing")
+    end
+
+    it "about" do
+      visit_page_with_content(visit_path: page_path("about"), content: "About")
+    end
+
+    it "faq" do
+      visit_page_with_content(visit_path: page_path("faq"), content: "Frequently Asked Questions")
+    end
+
+    it "terms" do
+      visit_page_with_content(visit_path: page_path("terms"), content: "Terms of Service")
+    end
+
+    it "privacy" do
+      visit_page_with_content(visit_path: page_path("privacy"), content: "Privacy Policy")
+    end
   end
 end
 
-def visit_page_with_content(path:, content:)
-  visit path
-  expect(page).to have_current_path(path)
+def visit_page_with_content(visit_path:, content:, result_path: nil)
+  visit visit_path
+  expect(page).to have_current_path(result_path || visit_path)
   expect(page).to have_content(content)
 end
