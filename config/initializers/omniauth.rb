e9600f2d7bd5b5a51db8cbffe6d395ca57111c24
Ricky Chilcott <ricky@rakefire.io>
2018-08-27 11:41:54 -0400

    Adding a Staging ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2,
-      ENV['GOOGLE_ID'],
-      ENV['GOOGLE_SECRET'],
+      ENV.fetch('GOOGLE_ID'),
+      ENV.fetch('GOOGLE_SECRET'),
       { scope: ENV['GOOGLE_SCOPE'],
         approval_prompt: "consent"}
 
--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,6 @@
 require 'rollbar'
 
 Rollbar.configure do |config|
-  config.enabled = Rails.env.production?
+  config.enabled = Rails.env.production? || Rails.env.staging?
   config.access_token = ENV.fetch("ROLLBAR_ACCESS_TOKEN")
 end
