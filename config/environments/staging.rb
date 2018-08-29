b490b4b079ba3a0d148c97c9c87162d1a8a3bf87
Ricky Chilcott <ricky@rakefire.io>
2018-08-29 10:29:28 -0400

    Turn off HTTP Auth in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,7 @@
 
 require_relative "production.rb"
 
+return
 Workflow::Application.configure do
   config.middleware.use ::Rack::Auth::Basic do |username, password|
     [username, password] == ["approveforme", "2018"]
