2c55ccf2868496bd7d6dc6bd745e174face44d20
Ricky Chilcott <ricky@rakefire.io>
2018-08-29 10:20:49 -0400

    Specify Rollbar environment

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,5 +2,6 @@ require 'rollbar'
 
 Rollbar.configure do |config|
   config.enabled = Rails.env.production? || Rails.env.staging?
+  config.environment = Rails.env
   config.access_token = ENV.fetch("ROLLBAR_ACCESS_TOKEN")
 end
