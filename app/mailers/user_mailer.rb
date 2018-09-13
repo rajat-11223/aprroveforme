4b7380b2e30ef2a810e3028ab992c7862ca349cb
Ricky Chilcott <ricky@rakefire.io>
2018-09-13 17:12:26 -0400

    Add UserMailer specs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,13 @@
 class UserMailer < ActionMailer::Base
-  default from: "\"ApproveForMe\" <Team@ApproveForMe.com>"
+  include ActionView::Helpers::SanitizeHelper
+  default from: "\"ApproveForMe\" <team@approveforme.com>"
+
   # mail sent when a user signs up for the service
-    def new_user(name, email)
-      @name = name
-      @email = email
-      mail(to: @email, subject: "Welcome to ApproveForMe!")
-    end
+  def new_user(name, email)
+    @name = name
+    @email = email
+    mail(to: @email, subject: "Welcome to ApproveForMe!")
+  end
 
   # mail sent to creator of a new approval
   def my_new_approval(approval)
@@ -13,7 +15,7 @@ class UserMailer < ActionMailer::Base
     @owner = @approval.user
     @name = @owner.name
     @email = @owner.email
-    @subject = @approval.title + " has been sent out for approval."
+    @subject = sanitize("#{@approval.title} has been sent out for approval")
     mail(to: @email, subject: @subject)
   end
 
@@ -21,19 +23,18 @@ class UserMailer < ActionMailer::Base
   def new_approval_invite(approval, approver)
     @approval = approval
     @owner = @approval.user
-    @subject = @owner.name + " has requested your approval on " + @approval.title
+    @subject = sanitize("#{@owner.name} has requested your approval on #{@approval.title}")
     @approver = approver
     @email = @approver.email
     mail(to: @email, subject: @subject, reply_to: @owner.email)
   end
-
 
   # mail sent whenever an approver responds
   def approval_update(approver)
     @approver = approver
     @approval = @approver.approval
     @owner = @approval.user
-    @subject = @approver.name + " has responded to " + @approval.title
+    @subject = sanitize("#{@approver.name} has responded to #{@approval.title}")
     mail(to: @owner.email, subject: @subject)
   end
 
@@ -41,7 +42,7 @@ class UserMailer < ActionMailer::Base
   def completed_approval(approval)
     @approval = approval
     @owner = @approval.user
-    @subject = @approval.title + " has been signed off!"
+    @subject = sanitize("#{@approval.title} has been signed off!")
     mail(to: @owner.email, subject: @subject)
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,85 @@
-require "spec_helper"
+require "rails_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when new user email" do
+    let(:user) { create(:user, first_name: "John", email: "john@test.com") }
+
+    it 'sends email' do
+      email = UserMailer.new_user(user.name, user.email)
+
+      expect(email.subject).to eq("Welcome to ApproveForMe!")
+      expect(email.to).to eq(["john@test.com"])
+      expect(email.from).to eq(["team@approveforme.com"])
+
+      expect(email.body.encoded).to include("Welcome")
+    end
+  end
+
+  context "when sending an approval, to owner" do
+    let(:user) { create(:user, first_name: "John", email: "john@test.com") }
+    let(:approval) { create(:approval, title: "<script>Payslip</script>", owner: user.id) }
+
+    it 'sends email' do
+      email = UserMailer.my_new_approval(approval)
+
+      expect(email.subject).to eq("Payslip has been sent out for approval")
+      expect(email.to).to eq(["john@test.com"])
+      expect(email.from).to eq(["team@approveforme.com"])
+
+      expect(email.body.encoded).to include("created a new approval")
+    end
+  end
+
+  context "when sending an approval, to approver" do
+    let(:user) { create(:user, name: "John", email: "john@test.com") }
+    let(:approval) { create(:approval, title: "<script>Payslip</script>", owner: user.id) }
+    let(:approver) { approval.approvers.first }
+
+    it 'sends email' do
+      email = UserMailer.new_approval_invite(approval, approver)
+
+      expect(email.subject).to eq("John has requested your approval on Payslip")
+      expect(email.to).to eq([approver.email])
+      expect(email.from).to eq(["team@approveforme.com"])
+      expect(email.reply_to).to eq(["john@test.com"])
+
+      expect(email.body.encoded).to include("has requested your approval of")
+    end
+  end
+
+  context "when an approver updates an approval" do
+    let(:user) { create(:user, name: "John", email: "john@test.com") }
+    let(:approval) { create(:approval, title: "<script>Payslip</script>", owner: user.id) }
+    let(:approver) { approval.approvers.first }
+
+    it 'sends email' do
+      approver.update_attributes status: "approved"
+
+      email = UserMailer.approval_update(approver)
+
+      expect(email.subject).to eq("#{approver.name} has responded to Payslip")
+      expect(email.to).to eq(["john@test.com"])
+      expect(email.from).to eq(["team@approveforme.com"])
+
+      expect(email.body.encoded).to include("Ricky Test has <b>approved</b>")
+    end
+  end
+
+  context "when an approval is complete" do
+    let(:user) { create(:user, name: "John", email: "john@test.com") }
+    let(:approval) { create(:approval, title: "<script>Payslip</script>", owner: user.id) }
+    let(:approver) { approval.approvers.first }
+
+    it 'sends email' do
+      approver.update_attributes status: "approved"
+
+      email = UserMailer.completed_approval(approval)
+
+      expect(email.subject).to eq("Payslip has been signed off!")
+      expect(email.to).to eq(["john@test.com"])
+      expect(email.from).to eq(["team@approveforme.com"])
+
+      expect(email.body.encoded).to include("has been signed off by all required approvers.")
+    end
+  end
 end
