8b03090aa61f3bb2b2eb177030ceeb94c9e62e02
Ricky Chilcott <ricky@rakefire.io>
2018-09-14 10:43:11 -0400

    Clean up routes and move around

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Workflow::Application.routes.draw do
-  post 'form_submission/create', as: :form_submission
-  get "pricing", as: :pricing, to: "pricing#index"
 
   resources :approvals do
-  	resources :approvers
+    resources :approvers
     resources :tasks
   end
 
@@ -11,39 +9,19 @@ Workflow::Application.routes.draw do
     collection do
       get :profile
 
+      # Payment management
       get :payment_methods
-      get :delete_payment_method
+      delete :delete_payment_method
       post :add_new_payment_method
-      get :set_default_payment_method
+      patch :set_default_payment_method
 
+      # Subscription management
       get :current_subscription
       get :subscription_histories
-      get :manage_subscription
-
-      get :active_approvals
-      get :completed_approvals
-
-      get :pending_approvals
-      get :signed_off_approvals
     end
   end
 
-  get "/#{Rails.application.config.google_verification}.html",
-      to: proc { |env| [200, {}, ["google-site-verification: #{Rails.application.config.google_verification}.html"]] }
-
-  constraints SignedIn.new { |user| user.user.has_role?(:admin) } do
-    mount Split::Dashboard, at: 'split'
-  end
-
-  get "/bomb", to: "errors#bomb"
-
   resources :users, only: [:index, :show, :edit, :update]
-
-  get '/auth/:provider/callback' => 'sessions#create', as: :oauth_callback
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
-  get '/getstarted/:intro' => 'home#index'
 
   resources :home do
     collection do
@@ -59,8 +37,29 @@ Workflow::Application.routes.draw do
     get :continue_permission, on: :collection
   end
 
+  # Form submission
+  post 'form_submission/create', as: :form_submission
+
+  # Admins-only
+  constraints SignedIn.new { |user| user.user.has_role?(:admin) } do
+    mount Split::Dashboard, at: 'split'
+  end
+
+  # Google Verification
+  get "/#{Rails.application.config.google_verification}.html",
+      to: proc { |env| [200, {}, ["google-site-verification: #{Rails.application.config.google_verification}.html"]] }
+
+  # Authentication
+  get '/auth/:provider/callback' => 'sessions#create', as: :oauth_callback
+  get '/signin' => 'sessions#new', :as => :signin
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/auth/failure' => 'sessions#failure'
+  get '/getstarted/:intro' => 'home#index'
+
   # Static Pages
+  get "pricing", as: :pricing, to: "pricing#index"
   get "/*id" => 'pages#show', as: :page, format: false
+  get "/bomb", to: "errors#bomb"
 
   root to: "home#index"
 end
