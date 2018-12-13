b55e2ae83a2bfacf589afb8b2b4f602d47fb6bf1
Ricky Chilcott <ricky@rakefire.io>
2018-12-12 23:03:12 -0500

    Remove ActionMailer::Base setting override

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,12 +6,14 @@ require_relative "production.rb"
 # https://github.com/omniauth/omniauth-oauth2/issues/58
 Rails.application.config.session_store :cookie_store, key: "_approveforme_session", domain: Workflow::Application::BASE_APP_DOMAIN, expire_after: 55.minutes
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  user_name: "0644e1881fb7c3",
-  password: "77b1fc73ce2060",
-  address: "smtp.mailtrap.io",
-  domain: "smtp.mailtrap.io",
-  port: "2525",
-  authentication: :cram_md5,
-}
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "0644e1881fb7c3",
+    password: "77b1fc73ce2060",
+    address: "smtp.mailtrap.io",
+    domain: "smtp.mailtrap.io",
+    port: "2525",
+    authentication: :cram_md5,
+  }
+end
