9081274c405bffad8ccfa2c79566baa321258c8e
Ricky Chilcott <ricky@rakefire.io>
2019-03-13 01:17:33 -0400

    Tell a user what they can do

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,2 +1,12 @@
 module ResponsesHelper
+  def approval_action_text(approval)
+    case approval.drive_perms.to_sym
+    when :writer
+      "edit"
+    when :commenter
+      "comment"
+    else
+      "view"
+    end
+  end
 end
